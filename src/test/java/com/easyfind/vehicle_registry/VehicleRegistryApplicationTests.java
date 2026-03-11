468038af2cf8150b043bdba6b6ae6107ff85af82
Junade <junade0708@gmail.com>
2026-03-11 16:41:28 +0200

    chore: boilerplate from initializr

---
+++ src/test/java/com/easyfind/vehicle_registry/VehicleRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyfind.vehicle_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
