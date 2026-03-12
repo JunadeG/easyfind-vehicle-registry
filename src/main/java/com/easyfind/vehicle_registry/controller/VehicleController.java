b1ba134bde10d7844f27196860b4f8d780ecda50
Junade <junade0708@gmail.com>
2026-03-12 15:50:09 +0200

    feat: implemented DTO pattern for vehicle response EF-V-104

--- src/main/java/com/easyfind/vehicle_registry/controller/VehicleController.java
+++ src/main/java/com/easyfind/vehicle_registry/controller/VehicleController.java
@@ -1,5 +1,6 @@
 package com.easyfind.vehicle_registry.controller;
 
+import com.easyfind.vehicle_registry.dto.VehicleResponseDTO;
 import com.easyfind.vehicle_registry.model.Vehicle;
 import com.easyfind.vehicle_registry.repository.VehicleRepository;
 import com.easyfind.vehicle_registry.service.VehicleService;
@@ -20,8 +21,8 @@ public class VehicleController {
     private VehicleService vehicleService;
 
     @GetMapping
-    public List<Vehicle> getAll() {
-        return vehicleRepository.findAll();
+    public List<VehicleResponseDTO> getAll() {
+        return vehicleService.getAllVehiclesDTO();
     }
 
     @PostMapping
